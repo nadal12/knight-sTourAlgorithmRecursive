8578aa7b872709d7426a06c1206c6aee70d5670d
nadal12 <nadal12@hotmail.com>
2018-12-14 16:27:56 +0100

    Inicialización del proyecto

--- Knight/src/knight/Knight.java
+++ Knight/src/knight/Knight.java
@@ -15,7 +15,8 @@ public class Knight {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+
     }
     
 }
